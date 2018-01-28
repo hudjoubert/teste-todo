53f4397f94338f3f0abee8d1fe7dd76249f3be15
hudjoubert <hudson_joubert@yahoo.com.br>
2018-01-28 21:56:15 -0200

    remover tasks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
 const ADD_TASK = 'ADD_TASK';
+const REMOVE_TASK = 'REMOVE_TASK';
 export const AddTask = (task) => {
     return {
         type: ADD_TASK,
+        payload: task
+    }
+}
+
+export const removeTask = (task) => {
+    return {
+        type: REMOVE_TASK,
         payload: task
     }
 }
--- src/reducers/TaskReducer.js
+++ src/reducers/TaskReducer.js
@@ -1,6 +1,6 @@
 const INITIAL_STATE = {
     tasks: [
-        'fazer compras'
+        'estudar'
     ]
 }
 
@@ -8,6 +8,11 @@ export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'ADD_TASK':
             state.tasks.push(action.payload)
+            return { ...state, tasks: state.tasks }
+            break;
+
+        case 'REMOVE_TASK':
+            state.tasks.splice(action.payload, 1)
             return { ...state, tasks: state.tasks }
             break;
 
